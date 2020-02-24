87f30ec42ce30be3778fab09ef12a2864bfae6a9
mariomtree <mario99manuel@gmail.com>
2020-02-25 00:22:59 +0200

    Leraning polymorphic many to many

--- polymorphic-manytomany/routes/web.php
+++ polymorphic-manytomany/routes/web.php
@@ -1,5 +1,8 @@
 <?php
-
+use App\Post;
+use App\User;
+use App\Video;
+use App\Tag;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +14,35 @@
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create',function(){
+	$post = Post::create(['name'=>'Lorem Lorem']);
+	$tag = Tag::find(2);
+	$post->tags()->save($tag);
+	$video = Video::create(['name'=>'video_01.mp4']);
+	$tag2 = Tag::find(3);
+	$video->tags()->save($tag2);
+});
+
+Route::get('/read',function(){
+	$post = Post::findOrFail(1);
+	foreach ($post->tags as $tag) {
+		echo $tag;
+	}
+});
+
+Route::get('/update',function(){
+	$post = Post::findOrFail(1);
+	foreach ($post->tags as $tag) 
+		return $tag->whereId(2)->update(['name'=>'Perl']);
+});
+
+Route::get('/delete', function(){
+	$post = Post::findOrFail(1);
+	foreach ($post->tags as $tag) 
+		return $tag->whereId(1)->delete();
 });
