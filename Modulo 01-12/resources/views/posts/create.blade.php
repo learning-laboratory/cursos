b6c02240c11d8ea709405d7a25e2eb896da548fa
mariomtree <mario99manuel@gmail.com>
2020-02-29 09:06:32 +0200

    Implementation laravel collection in create and edit page

--- Modulo 01-12/resources/views/posts/create.blade.php
+++ Modulo 01-12/resources/views/posts/create.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-	<form method="post" action="/posts">
-		<input type="text" name="title" placeholder="Enter title">
-		{{ csrf_field() }}
-		<button type="submit" name="submit">Send</button>
-	</form>
+	<h1>Create Post</h1>
+	<!--<form method="post" action="/posts">-->
+	{!! Form::Open(['method'=>'POST','action'=>'PostController@store']) !!}
+		
+		<div class="form-group">
+			{!! Form::label('title','Title:') !!}
+			{!! Form::text('title',null,['class'=>'form-control']) !!}
+		</div>
+		{!! Form::submit('save',['class'=>'btn btn-danger']) !!}
+
+	{!! Form::close() !!}
+@endsection
 @yield('footer')
--- Modulo 01-12/resources/views/posts/edit.blade.php
+++ Modulo 01-12/resources/views/posts/edit.blade.php
@@ -3,16 +3,17 @@
 <h2>Edit Post</h2>
 
 @section('content')
-	<form method="post" action="/posts/{{$post->id}}">
-		
-		<input type="hidden" name="_method" value="PUT">
-		<input type="text" name="title" placeholder="Enter title" value="{{$post->title}}">
-		{{ csrf_field() }} 
-		<button type="submit" name="submit">Update</button>
-	</form>
-	<form method="post" action="/posts/{{$post->id}}">
-		<input type="hidden" name="_method" value="DELETE">
-		{{ csrf_field() }} 
-		<button type="submit">Delete</button>
-	</form>
+	{!! Form::model($post,['method'=>'PATCH','action'=>['PostController@update',$post->id] ]) !!}
+		<div class="form-group">
+			{{ csrf_field() }} 
+			{!! Form::label('title','Title:') !!}
+			{!! Form::text('title',null,['class'=>'form-control']) !!}
+		</div>
+		{!! Form::submit('update',['class'=>'btn btn-danger']) !!}
+	{!! Form::close() !!}
+	
+	{!! Form::open(['method'=>'DELETE','action'=>['PostController@destroy',$post->id]]) !!}
+		{{ csrf_field() }} 	
+		{!! Form::submit('delete',['class'=>'btn btn-danger']) !!}
+	{!! Form::close() !!}
 @yield('footer')
