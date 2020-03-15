f77b6fbe957025d7668cefa0ff469f5167cca863
mariomtree <mario99manuel@gmail.com>
2020-03-15 19:46:52 +0200

    Add file js, css

--- app/routes/web.php
+++ app/routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::auth();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('admin/users','AdminUserController');
+
+
+
