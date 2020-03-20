7e79e67d710518577a94b2effcf4cba975fa8a56
mariomtree <mario99manuel@gmail.com>
2020-03-20 10:24:21 +0200

    display users in dashboard

--- app/app/Http/Controllers/AdminUserController.php
+++ app/app/Http/Controllers/AdminUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class AdminUserController extends Controller
 {
@@ -13,7 +14,8 @@ class AdminUserController extends Controller
      */
     public function index()
     {
-        return view('admin.users.index');
+        $users = User::all();
+        return view('admin.users.index',compact('users'));
     }
 
     /**
