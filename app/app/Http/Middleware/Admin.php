c9bfd55d10bad2378999b8836bffca3076aa1698
mariomtree <mario99manuel@gmail.com>
2020-03-24 12:43:15 +0200

    Update Admin.php

--- app/app/Http/Middleware/Admin.php
+++ app/app/Http/Middleware/Admin.php
@@ -16,9 +16,11 @@ class Admin
      */
     public function handle($request, Closure $next)
     {
-       // if (Auth::check()) {
-          //  if (Auth::user()->isAdmin()) {
+        if (Auth::check()) {
+            if (Auth::user()->isAdmin()) {
                 return $next($request);
-      
+            }
+        }
+     return redirect('/');
     }
 }
