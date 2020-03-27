1865e587c964dd15949d51a56c9e03801bf8e8cf
mariomtree <mario99manuel@gmail.com>
2020-03-27 16:28:51 +0200

    Add pagination in dashboard for posts

--- app/app/Http/Controllers/AdminPostsController.php
+++ app/app/Http/Controllers/AdminPostsController.php
@@ -21,7 +21,7 @@ class AdminPostsController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::paginate(2);
         return view('admin.posts.index',compact('posts'));
     }
 
@@ -134,3 +134,4 @@ class AdminPostsController extends Controller
     }
 
 }
+?>
