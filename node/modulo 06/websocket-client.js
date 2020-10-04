11996c40e6252ee2af9c6b67184e32727e53a1bd
Mario Manuel Mabande <54041357+mariomthree@users.noreply.github.com>
2020-10-04 07:57:32 +0200

    Aula 34 - Enviando dados

--- node/modulo 06/websocket-client.js
+++ node/modulo 06/websocket-client.js
@@ -1,1 +1,6 @@
 const WS = new WebSocket('ws://localhost:9000');
+document.forms[0].onSubmit = () => {
+    let input = document.getElementsByName('message');
+    console.log(input.value);
+    WS.send(input.value)
+};
--- node/modulo 06/websocket-server.js
+++ node/modulo 06/websocket-server.js
@@ -2,5 +2,8 @@ const WebSocketServer = require('ws').Server;
 const WSS = new WebSocketServer({ port: 9000 });
 
 WSS.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        console.log(message);
+    });
     console.log("Connected.");
 });
