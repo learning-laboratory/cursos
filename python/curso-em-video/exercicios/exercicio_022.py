457fbbbde5ae23fcada8b86398267ff04b8f1af5
Mario Manuel Mabande <54041357+mariomthree@users.noreply.github.com>
2021-01-10 18:58:28 +0200

    Introducao a React

--- python/curso-em-video/exercicios/exercicio_022.py
+++ python/curso-em-video/exercicios/exercicio_022.py
@@ -3,7 +3,7 @@ name = input('Type your name: ')
 lenghtName = len(name.replace(' ', ''))
 firstName = name.split()[0]
 
-print('UPPERCASE: {}'.format(name.upper()))
+print('UPPERCASE: {}'.format(name.replace(' ', '')))
 print('lowercase: {}'.format(name.lower()))
 print('length of name: {}'.format(lenghtName))
 print('lenght of first name: {}'.format(len(firstName)))
