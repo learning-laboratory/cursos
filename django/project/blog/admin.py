fa5e01b27f0fd3f2e2c55ee639cc9703ca06350b
mariomthree <mario99manuel@gmail.com>
2021-01-13 08:56:19 +0200

    create superuser, views, urls and custumize views

--- django/project/blog/admin.py
+++ django/project/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+#admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "author", "created_at", "updated_at")
+    prepopulated_fields = {"slug": ("title",)}
