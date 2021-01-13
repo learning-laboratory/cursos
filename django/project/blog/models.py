b0df74a10571f0a6b395974243d7fa011bae3c11
mariomthree <mario99manuel@gmail.com>
2021-01-13 11:26:32 +0200

    Create template view

--- django/project/blog/models.py
+++ django/project/blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -9,5 +10,11 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
 
+    class Meta:
+        ordering = ("-created_at", )
+
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:detail", kwargs={"slug":self.slug})
