0c55b3405961d551b16d206d9c0cb211afb65bd8
mariomthree <mari99manuel@gmail.com>
2021-01-19 14:53:51 +0200

    learning diffent type of fields model

--- django/trydjango/src/products/models.py
+++ django/trydjango/src/products/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name        = models.TextField()
-    description = models.TextField()
-    price       = models.TextField()
+    name        = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price       = models.DecimalField(decimal_places=2, max_digits=100)
     summary     = models.TextField(default="Enter product summary!")
     
     def __str__(self):
