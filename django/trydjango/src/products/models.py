777566def8ef45ec010073aeffd8dc46cc801a31
mariomthree <mari99manuel@gmail.com>
2021-01-20 09:03:48 +0200

    changing value of  attribute

--- django/trydjango/src/products/models.py
+++ django/trydjango/src/products/models.py
@@ -5,7 +5,8 @@ class Product(models.Model):
     name        = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     price       = models.DecimalField(decimal_places=2, max_digits=10000)
-    summary     = models.TextField(default="Enter product summary!")
+    summary     = models.TextField(blank=True, null=False)
+    featured    = models.BooleanField() # null=True, default=True
     
     def __str__(self):
         return self.name
