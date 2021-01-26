f74b39f5b042c1dd5d7168a79f03772806b4b5c0
mariomthree <mari99manuel@gmail.com>
2021-01-26 13:41:38 +0200

    learning how raw html form

--- django/trydjango/src/products/views.py
+++ django/trydjango/src/products/views.py
@@ -3,15 +3,22 @@ from .models import Product
 from .forms import ProductForm
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-        
-    context = {
-        'form': form
-    }
+
+    print(request.GET['name'])
+    print(request.POST.get("name"))
+    context = {}
     return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+        
+#     context = {
+#         'form': form
+#     }
+#     return render(request, "products/product_create.html", context)
 
 
 def product_detail_view(request):
