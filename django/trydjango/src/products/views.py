921b0e5ed762f64373087e2ea83ef8e41269bded
mariomthree <mari99manuel@gmail.com>
2021-01-27 13:41:02 +0200

    update url for initial values forms

--- django/trydjango/src/products/views.py
+++ django/trydjango/src/products/views.py
@@ -2,24 +2,24 @@ from django.shortcuts import render
 from .models import Product
 from .forms import ProductForm, RawProductForm
 
-# def product_create_view(request):
+def product_create_view(request):
 
-#     form = RawProductForm()
-#     if request.method == "POST":
-#         form = RawProductForm(request.POST)
-#         if form.is_valid():
-#             # The data is good
-#             print(form.cleaned_data)
-#             Product.objects.create(
-#                 **form.cleaned_data
-#             )
-#         else:
-#             print(form.errors)
+    form = RawProductForm()
+    if request.method == "POST":
+        form = RawProductForm(request.POST)
+        if form.is_valid():
+            # The data is good
+            print(form.cleaned_data)
+            Product.objects.create(
+                **form.cleaned_data
+            )
+        else:
+            print(form.errors)
 
-#     context = {
-#         "form": form
-#     }
-#     return render(request, "products/product_create.html", context)
+    context = {
+        "form": form
+    }
+    return render(request, "products/product_create.html", context)
 
 # def product_create_view(request):
 
