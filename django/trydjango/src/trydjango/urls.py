f49ab377bce2840d65cd2d59f299c5ca81b4e5c9
mariomthree <mari99manuel@gmail.com>
2021-02-01 13:19:21 +0200

    create blog and class based views - listViews

--- django/trydjango/src/trydjango/urls.py
+++ django/trydjango/src/trydjango/urls.py
@@ -25,13 +25,12 @@ from pages.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    
+    path('blog/', include("blog.urls")),
     path('products/', include('products.urls')),
 
     path('', home_view, name="home"),
     path('contact', contact_view, name="contact"),
     path('about', about_view, name="about"),
     path('social', social_view, name="social"),
-
-
 ]
