87c7b8a4e51c8c2fbef76a6863ce103df157d84b
mariomthree <mario99manuel@gmail.com>
2021-03-12 15:01:37 +0200

    fix erro in meta class model

--- django/locallibrary/locallibrary/catalog/models.py
+++ django/locallibrary/locallibrary/catalog/models.py
@@ -4,8 +4,8 @@ import uuid
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
-    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    summary = models.TextField(max_length=1000,null=True, help_text='Enter a brief description of the book')
+    isbn = models.CharField('ISBN',null=True, max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField('Genre', help_text='Select a genre for this book')
     language = models.ForeignKey('language', on_delete=models.SET_NULL, null=True)
 
@@ -43,7 +43,7 @@ class BookInstance(models.Model):
         ordering = ['due_back']
     
     def __str__(self):
-        return f'{self.id} ({self.name})'
+        return self.book
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
@@ -55,7 +55,7 @@ class Author(models.Model):
         ordering = ['first_name']
     
     def __str__(self):
-        return self.name
+        return self.first_name
     
     def get_absolute_url(self):
         return reverse("author_detail", kwargs={"id": self.id})
