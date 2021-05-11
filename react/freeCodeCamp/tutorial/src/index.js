29515b67e5222e4f6c414736e58438d87e24f810
Mario Manuel Mabande <mario99manuel@gmail.com>
2021-05-11 18:11:51 +0200

    key prop and spread operator

--- react/freeCodeCamp/tutorial/src/index.js
+++ react/freeCodeCamp/tutorial/src/index.js
@@ -4,11 +4,13 @@ import './index.css'
 
 const books = [
   {
+    id:1,
     img :'https://www.mail-signatures.com/wp-content/uploads/2019/02/How-to-find-direct-link-to-image_Blog-Picture.png',
     title:"A litle smill",
     author:"James Piter"
   },
   {
+    id:2,
     img :'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/July/amazonbasics_520x520._SY304_CB442725065_.jpg',
     title:"The Big Bang",
     author:"Mario Manuel"
@@ -20,7 +22,7 @@ function BookList(){
       <section className="booklist">
         {
           books.map((book)=>{
-            return (<Book book={book}/>)
+            return (<Book key={book.id} {...book}/>)
           })
         }
       </section>
@@ -28,7 +30,7 @@ function BookList(){
 }
 
 const Book = (props) => {
-  const {img, title, author} = props.book
+  const {img, title, author} = props
   return (
     <article className="book">
       <img src={img} alt=""/>
