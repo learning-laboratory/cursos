4ba636f8c2646409f7d59323d0861fb1f1909915
Mario Manuel Mabande <mario99manuel@gmail.com>
2021-05-12 15:28:36 +0200

    import and export

--- react/freeCodeCamp/tutorial/src/index.js
+++ react/freeCodeCamp/tutorial/src/index.js
@@ -1,23 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
+import { books } from './books'
+import Book from './Book'
+import { greeting } from './tests/test'
 
-const books = [
-  {
-    id:1,
-    img :'https://www.mail-signatures.com/wp-content/uploads/2019/02/How-to-find-direct-link-to-image_Blog-Picture.png',
-    title:"A litle smill",
-    author:"James Piter"
-  },
-  {
-    id:2,
-    img :'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/July/amazonbasics_520x520._SY304_CB442725065_.jpg',
-    title:"The Big Bang",
-    author:"Mario Manuel"
-  }
-]
 
 function BookList(){
+  console.log(greeting)
   return (
       <section className="booklist">
         {
@@ -26,32 +16,6 @@ function BookList(){
           })
         }
       </section>
-  )
-}
-
-const Book = (props) => {
-
-  const {img, title, author} = props
-  const clickHandler = (e) => {
-    console.log(e)
-    console.log(e.target)
-    alert("Hello")
-  }
-
-  const complexExample = (author) => {
-    console.log(author)
-  }
-
-  return (
-    <article className="book" onMouseOver={() => {
-      console.log(title)
-    }}>
-      <img src={img} alt=""/>
-      <h1 onClick={() => console.log(title)} >{title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>reference example</button>
-      <button type="button" onClick={() => complexExample(author)}>complex example</button>
-    </article>
   )
 }
 
