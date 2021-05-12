26dd023715ed4961525726d132c2b593721cb4a6
Mario Manuel Mabande <mario99manuel@gmail.com>
2021-05-12 15:08:02 +0200

    events

--- react/freeCodeCamp/tutorial/src/index.js
+++ react/freeCodeCamp/tutorial/src/index.js
@@ -21,7 +21,7 @@ function BookList(){
   return (
       <section className="booklist">
         {
-          books.map((book)=>{
+          books.map((book) => {
             return (<Book key={book.id} {...book}/>)
           })
         }
@@ -30,12 +30,27 @@ function BookList(){
 }
 
 const Book = (props) => {
+
   const {img, title, author} = props
+  const clickHandler = (e) => {
+    console.log(e)
+    console.log(e.target)
+    alert("Hello")
+  }
+
+  const complexExample = (author) => {
+    console.log(author)
+  }
+
   return (
-    <article className="book">
+    <article className="book" onMouseOver={() => {
+      console.log(title)
+    }}>
       <img src={img} alt=""/>
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)} >{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>reference example</button>
+      <button type="button" onClick={() => complexExample(author)}>complex example</button>
     </article>
   )
 }
