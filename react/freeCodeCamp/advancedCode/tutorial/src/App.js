462ae943da89b9690ec3423782c3caa07ac8a0e4
Mario Manuel Mabande <mario99manuel@gmail.com>
2021-05-12 18:10:52 +0200

    simple useState

---
+++ react/freeCodeCamp/advancedCode/tutorial/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Setup from './tutorial/1-useState/setup/1-error-example'
+
+function App() {
+  return (
+    <div className='container'>
+        <h2><Setup/></h2>
+    </div>
+  );
+}
+
+export default App;
