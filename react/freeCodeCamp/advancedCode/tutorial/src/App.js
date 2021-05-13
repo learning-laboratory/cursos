eb8ee4f9e7307d86dae8a416bdbd1a3486408d67
Mario Manuel Mabande <mario99manuel@gmail.com>
2021-05-13 13:04:56 +0200

    useState array

--- react/freeCodeCamp/advancedCode/tutorial/src/App.js
+++ react/freeCodeCamp/advancedCode/tutorial/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-// import Setup from './tutorial/1-useState/setup/1-error-example'
-import Setup from './tutorial/1-useState/setup/2-useState-basics'
+//import Setup from './tutorial/1-useState/setup/1-error-example'
+//import Setup from './tutorial/1-useState/setup/2-useState-basics'
+import Setup from './tutorial/1-useState/setup/3-useState-array'
 
 function App() {
   return (
     <div className='container'>
-        <h2><Setup/></h2>
+      <h2><Setup/></h2>
     </div>
   );
 }
