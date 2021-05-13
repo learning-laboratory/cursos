d3132055c030b4748f3aeeadaa81dd7a17685a18
Mario Manuel Mabande <mario99manuel@gmail.com>
2021-05-13 12:06:01 +0200

    UseState basics

--- react/freeCodeCamp/advancedCode/tutorial/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react/freeCodeCamp/advancedCode/tutorial/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('Hello World'))
+  // const value = useState(1)[0]
+  // const handler = useState(1)[1]
+  // console.log(value, handler)
+  const [text, setText] = useState('random title!')
+  const handlerClick = () => {
+    if(text === 'random title!')
+      setText('hello world!')
+    else
+      setText('random title!')
+  } 
+  return (
+    <React.Fragment>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+      <button className="btn" onClick={handlerClick}>change title</button>
+    </React.Fragment>
+  )
 };
 
-export default UseStateBasics;
+export default UseStateBasics
