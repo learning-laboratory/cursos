b89a93729921dd55757d7925b4360bfadbdbf2dc
Mario Manuel Mabande <mario99manuel@gmail.com>
2021-05-14 08:31:32 +0200

    functional update form

--- react/freeCodeCamp/advancedCode/tutorial/src/tutorial/1-useState/setup/3-useState-array.js
+++ react/freeCodeCamp/advancedCode/tutorial/src/tutorial/1-useState/setup/3-useState-array.js
@@ -5,8 +5,13 @@ const UseStateArray = () => {
   const [people, setPeople] = React.useState(data)
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id)
-    setPeople(newPeople)
+    // let newPeople = people.filter((person) => person.id !== id)
+    // setPeople((newPeople))
+
+    setPeople((oldPeople) => {
+      let newPeople =  oldPeople.filter((person) => person.id !== id)
+      return newPeople
+    })
   }
   return (
     <>
