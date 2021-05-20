2e0ab8e4b4c786f5df0715ecc88e442a441dffbd
Mario Manuel Mabande <mario99manuel@gmail.com>
2021-05-20 13:25:13 +0200

    short circuit

--- react/freeCodeCamp/advancedCode/tutorial/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react/freeCodeCamp/advancedCode/tutorial/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  
+  const [text, setText] = useState('Music')
+  const firstValue = text || 'hello world'
+  const secondValue = text && 'hello world'
 
-  return <h2>short circuit</h2>;
-};
+  const [isError, setError] = useState(false)
 
-export default ShortCircuit;
+  return (
+    <>
+      {/* <h2>{firstValue}</h2> */}
+      {/* <h1>{secondValue}</h1> */}
+      <h1>{text || 'John Doe' }</h1>
+      <button className="btn" onClick={() => setError(!isError)}>Toggle Error</button>
+      <h2>{!text && 'Hello Word'}</h2>
+      <h2>{text && 'Hello Word, Too'}</h2>
+      <h2>{isError && 'Error page'}</h2>
+    </>
+  )
+}
+
+export default ShortCircuit
